c52d76c862efd74f7649ba38e40e6b8165c12b2f
rs2cat-pr <93893048+rs2cat-pr@users.noreply.github.com>
2021-11-18 00:45:36 +0900

    新規Todoを未完了リストに追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import "./styles.css";
+
+/**
+ * 追加機能
+ */
+//1.追加ボタンを押したときにフォームの値を取得する
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  //関数に保持した後は、フォームの内容を破棄する
+  document.getElementById("add-text").value = "";
+
+  //2.未完了のDOMを生成し、HTMLへ挿入する
+  /*
+ 生成する内容
+ <li>
+<span>inputText</span>
+<button>done</button>
+<button>delete</button>
+</li>
+ */
+  const li = document.createElement("li");
+  li.className = "list-content";
+  const span = document.createElement("span");
+  span.innerText = inputText;
+
+  //buttonタグ（完了）の作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "done";
+  //buttonタグ(削除）の作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
+
+  //liタグの子要素に各要素を詰める
+  li.appendChild(span);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+//追加ボタンが押されたときにonClickAdd関数が発動する
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
